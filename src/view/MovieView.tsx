5e53ba6029d82429a94e7b0c18397421a1428605
Kicks-Me <saiyang2015@gmail.com>
2025-06-18 14:19:16 +0700

    ddd

--- src/view/MovieView.tsx
+++ src/view/MovieView.tsx
@@ -1,4 +1,4 @@
-import { Button, Card, Col, Empty, Flex, Image, notification, Pagination, Rate, Row, Space, Spin, Typography } from 'antd';
+import { Button, Card, Col, Empty, Flex, Image, notification, Pagination, Rate, Row, Select, Skeleton, Space, Spin, Typography } from 'antd';
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import type { IMovie, IResult } from '../interface/mv.type';
@@ -15,13 +15,14 @@ const MovieView: React.FC = (props: Props) => {
   const [currentPage, setCurrentPage] = useState<number>(1);
   const [pageSize, setPageSize] = useState<number>(10);
   const [bg, setBg] = useState("")
+  const [genres, setGenres] = useState(28)
 
   const getMovie = async (page: number = 1) => {
     const url = import.meta.env.VITE_API_END_POINT;
 
     try {
       setLoading(true);
-      const result = await axios.get(url); 
+      const result = await axios.get(`${url}&with_genres=${genres}`); 
       setMvData(result?.data || null);
     } catch (error) {
       setMvData(null);
@@ -53,6 +54,11 @@ const MovieView: React.FC = (props: Props) => {
     }
   };
 
+  const onChange = (value: number) => {
+    console.log(`selected ${value}`);
+    setGenres(value)
+  };
+
 useEffect(() => {
   if (paginatedResults.length > 0) {
     const timer = setTimeout(() => {
@@ -65,6 +71,12 @@ useEffect(() => {
   }
 }, [paginatedResults]);
 
+useEffect(()=>{
+    if(genres){
+        getMovie();
+    }
+}, [genres]);
+
   return (
     <Content>
       <Content
@@ -76,10 +88,17 @@ useEffect(() => {
       <Flex justify="space-between" align="center" className="!px-5 md:!px-10 lg:!px-20 !py-10">
         <Typography.Title level={2}>Movie List</Typography.Title>
         <Space className="cursor-pointer">
-          <FaListUl size={22} />
+          Filter:
+          <Select onChange={onChange} className='!w-44 !ml-2'>
+                <Select.Option value="28">TV movie</Select.Option>
+                <Select.Option value="36">Sicient Fiction</Select.Option>
+                <Select.Option value="10402">MV History</Select.Option>
+                <Select.Option value="878">MV Action</Select.Option>
+                <Select.Option value="10770">Music</Select.Option>
+            </Select>
         </Space>
       </Flex>
-      <Spin size="large" spinning={loading} tip="ກຳລັງໂຫລດຂໍ້ມູນ..." className="!border">
+      <Skeleton loading={loading} className="!border">
         <Row gutter={[24, 24]} className="!p-5 md:!p-10 lg:!p-20">
           {paginatedResults.length > 0 ? (
             paginatedResults.map((item: IResult, index: number) => (
@@ -102,7 +121,7 @@ useEffect(() => {
             />
           </Flex>
         )}
-      </Spin>
+      </Skeleton>
     </Content>
   );
 };
